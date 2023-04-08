08e9d9d2312c0721df51a967939438286db17b57
AhamedAfq <ashfakahamed142@gmail.com>
2023-04-08 11:40:57 +0530

    Controller, static resources and template setup

--- src/main/java/com/ContactManager/controller/HomeController.java
+++ src/main/java/com/ContactManager/controller/HomeController.java
@@ -4,7 +4,9 @@ import com.ContactManager.dao.UserRepository;
 import com.ContactManager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,13 +15,24 @@ public class HomeController  {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/test")
-    @ResponseBody
-    public String test(){
-        User user = new User();
-        user.setName("AhamedAfq");
-        user.setEmail("ashfakahamed142@gmail.com");
-        userRepository.save(user);
-        return "Its Working...";
+//    @GetMapping("/test")
+//    @ResponseBody
+//    public String test(){
+//        User user = new User();
+//        user.setName("AhamedAfq");
+//        user.setEmail("ashfakahamed142@gmail.com");
+//        userRepository.save(user);
+//        return "Its Working...";
+//    }
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
+    }
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
     }
 }

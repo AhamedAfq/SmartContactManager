0e384e32083b01ca759e74c258d37e0ca4c5a3b3
AhamedAfq <ashfakahamed142@gmail.com>
2023-04-25 17:50:23 +0530

    Add contact form changes

--- src/main/java/com/ContactManager/controller/UserController.java
+++ src/main/java/com/ContactManager/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.ContactManager.controller;
 
 import com.ContactManager.dao.UserRepository;
+import com.ContactManager.entities.Contact;
 import com.ContactManager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,17 +19,30 @@ public class UserController  {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashBoard(Model model, Principal principal){  // Principal return the unique attribute of the Entity
+    @ModelAttribute // Common set of code that has run before all the following handlers (or) method to add common data to teh response
+    public void addCommonData(Model model, Principal principal){ // Principal return the unique attribute of the Entity
         String userName = principal.getName();
         System.out.println("USERNAME: " + userName);
-//      Get the user using userName
 
+        //      Get the user using userName
         User user = userRepository.getUserByUserName(userName);
         System.out.println("USER: " + user);
 
         model.addAttribute("user", user);
+    }
 
+    @RequestMapping("/index")
+    public String dashBoard(Model model, Principal principal){  // Principal return the unique attribute of the Entity
+        model.addAttribute("title", "User dashboard");
         return "normal/user_dashboard";
+    }
+
+//    Open add form handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model){
+
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 }

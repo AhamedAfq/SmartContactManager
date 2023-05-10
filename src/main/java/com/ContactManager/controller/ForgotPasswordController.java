329749bc96131fd202138fdc1ac7398b12b08267
AhamedAfq <ashfakahamed142@gmail.com>
2023-05-10 12:05:45 +0530

    Verify OTP and User Email while changing password

--- src/main/java/com/ContactManager/controller/ForgotPasswordController.java
+++ src/main/java/com/ContactManager/controller/ForgotPasswordController.java
@@ -1,5 +1,7 @@
 package com.ContactManager.controller;
 
+import com.ContactManager.dao.UserRepository;
+import com.ContactManager.entities.User;
 import com.ContactManager.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,9 @@ public class ForgotPasswordController {
 
     @Autowired
     private EmailService emailService;
+
+    @Autowired
+    private UserRepository userRepository;
     @RequestMapping("/forgot")
     public String openEmailForm(){
         return "forgot_email_form";
@@ -32,16 +37,60 @@ public class ForgotPasswordController {
         System.out.println("OTP triggered: "+ otp);
 //TODO:  Code for send OTR to email
         String subject = "OTP from smart contact manager";
-        String message = "OTP = "+otp+"";
+        String message = ""
+                + "<div style='border:1px solid #e2e2e2; padding:20px'>"
+                + "<h1>"
+                + "OTP is "
+                + "<b>"+otp
+                + "</n>"
+                + "</h1> "
+                + "</div>";;
         String to = email;
 
         boolean flag = this.emailService.sendEmail(subject, message, to);
         if(flag){
             session.setAttribute("otp", otp);
+            session.setAttribute("email", email);
             return "verify_otp";
         }else{
             session.setAttribute("message", "Pls check your email Id !!!");
-            return "change_password";
+            return "forgot_email_form";
         }
     }
+
+//    Verify OTP controller
+    @PostMapping("/verify-otp")
+    public String verifyOtp(@RequestParam("otp") int otp,HttpSession session)
+    {
+        int myOtp=(int)session.getAttribute("otp");
+
+        System.out.println("User OTP "+otp);
+        System.out.println("Our OTP "+myOtp);
+
+        String email=(String)session.getAttribute("email");
+        if(myOtp==otp)
+        {
+            //password change form
+            User user = this.userRepository.getUserByUserName(email);
+
+            if(user==null)
+            {
+                //send error message
+                session.setAttribute("message", "User does not exits with this email !!");
+
+                return "forgot_email_form";
+            }else
+            {
+                //send change password form
+                return "password_change_form";
+
+            }
+        }else
+        {
+            session.setAttribute("message", "You have entered wrong otp !!");
+            return "verify_otp";
+        }
+    }
+
+
 }
--- src/main/java/com/ContactManager/service/EmailService.java
+++ src/main/java/com/ContactManager/service/EmailService.java
@@ -4,12 +4,15 @@ package com.ContactManager.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class EmailService {
@@ -19,15 +22,29 @@ public class EmailService {
     public boolean sendEmail(String subject, String message, String to) {
 
         boolean flag = false;
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("secularcomerade@gmail.com");
-        mailMessage.setTo(to);
-        mailMessage.setText(message);
-        mailMessage.setSubject(subject);
+//        SimpleMailMessage mailMessage = new SimpleMailMessage();
+//        mailMessage.setFrom("secularcomerade@gmail.com");
+//        mailMessage.setTo(to);
+//        mailMessage.setText(message);
+//        mailMessage.setSubject(subject);
+//
+//        javaMailSender.send(mailMessage);
+//        flag = true;
+//        System.out.println("Mail Sent Successfully");
 
-        javaMailSender.send(mailMessage);
-        flag = true;
-        System.out.println("Mail Sent Successfully");
+        try{
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            mimeMessage.setSubject(subject);
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom("ashfakahamed11@gmail.com");
+            helper.setTo(to);
+            helper.setText(message, true);
+            javaMailSender.send(mimeMessage);
+            flag = true;
+        }catch (Exception exception){
+            Logger.getLogger(EmailService.class.getName()).log(Level.SEVERE, null, exception);
+        }
+        System.out.println("Flag: "+flag);
         return flag;
     }
 }
